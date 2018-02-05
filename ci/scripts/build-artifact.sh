97f10b935bfacfb899906a334d3d7ef111db4822
dambor <glenioborges@gmail.com>
2018-02-05 13:08:47 -0200

    included version

--- ci/scripts/build-artifact.sh
+++ ci/scripts/build-artifact.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 set -xe
 
+version=`cat version/number`
 cd git-repo/ola-nativos
 ./mvnw install
-cp target/*.jar ../artifact-dir/${base_name}*.jar
+cp target/*.jar ../artifact-dir/${base_name}-${version}.jar
 ls ../artifact-dir
