ff325e951425f2471026c6f9a59ff0854ab4a91d
dambor <glenioborges@gmail.com>
2018-03-20 19:04:23 -0300

    ola-nativos

---
+++ ola-nativos/src/main/java/io/pivotal/olanativos/OlaNativosApplication.java
@@ -0,0 +1,24 @@
+package io.pivotal.olanativos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class OlaNativosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OlaNativosApplication.class, args);
+	}
+}
+
+@RestController
+class GreetingRestController {
+
+	@GetMapping("/ola")
+
+	String ola() {
+		return "Oi pessoal do DF!";
+	}
+}
---
+++ ola-nativos/src/test/java/io/pivotal/olanativos/OlaNativosApplicationTests.java
@@ -0,0 +1,16 @@
+package io.pivotal.olanativos;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OlaNativosApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
