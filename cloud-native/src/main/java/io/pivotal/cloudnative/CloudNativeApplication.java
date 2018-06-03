4601dd5d989080e5d74040a5dd17432b8a339041
dambor <glenioborges@gmail.com>
2018-06-03 17:33:12 -0300

    cloud-native

--- cloud-native/src/main/java/io/pivotal/cloudnative/CloudNativeApplication.java
+++ cloud-native/src/main/java/io/pivotal/cloudnative/CloudNativeApplication.java
@@ -1,24 +1,25 @@
-package io.pivotal.cloudnativegdb;
+package io.pivotal.cloudnative;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-public class CloudNativeGdbApplication {
+public class CloudNativeApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CloudNativeGdbApplication.class, args);
+		SpringApplication.run(CloudNativeApplication.class, args);
 	}
 }
 
 @RestController
 class GreetingsRestController {
 
-	@RequestMapping("/welcome")
+	@GetMapping("/welcome")
 
 	String welcome() {
-		return "Welcome to cloud-native architecture!";
+		return "Welcome to cloud-native";
 	}
 }
--- cloud-native/src/test/java/io/pivotal/cloudnative/CloudNativeApplicationTests.java
+++ cloud-native/src/test/java/io/pivotal/cloudnative/CloudNativeApplicationTests.java
@@ -1,4 +1,4 @@
-package io.pivotal.cloudnativegdb;
+package io.pivotal.cloudnative;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class CloudNativeGdbApplicationTests {
+public class CloudNativeApplicationTests {
 
 	@Test
 	public void contextLoads() {
