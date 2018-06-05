462b228a871de5c4ce5129a9d08943007bc4527d
dambor <glenioborges@gmail.com>
2018-06-05 10:06:49 -0300

    cloud-native

---
+++ cloud-native/src/main/java/io/pivotal/cloudnative/CloudNativeApplication.java
@@ -0,0 +1,24 @@
+package io.pivotal.cloudnative;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class CloudNativeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudNativeApplication.class, args);
+	}
+}
+
+@RestController
+class GreetingsRestController {
+
+	@GetMapping("/welcome")
+
+	String welcome(){
+		return "Welcome to Cloud Native!";
+	}
+}
